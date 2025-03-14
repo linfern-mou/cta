9aa038a3cea1b41c859b33d0dc28f4bd2845b53d
lushunming <1357197829@qq.com>
2025-03-14 09:24:12 +0800

    测试ucToken

--- app/src/main/java/com/github/catvod/spider/Introduce.java
+++ app/src/main/java/com/github/catvod/spider/Introduce.java
@@ -1,11 +1,14 @@
 package com.github.catvod.spider;
 
 import android.content.Context;
+import com.github.catvod.api.QRCodeHandler;
+import com.github.catvod.bean.Class;
 import com.github.catvod.bean.Result;
 import com.github.catvod.bean.Vod;
 import com.github.catvod.crawler.Spider;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Introduce extends Spider {
@@ -19,6 +22,8 @@ public class Introduce extends Spider {
 
     @Override
     public String homeContent(boolean filter) throws Exception {
+        List<Class> classes = new ArrayList<>();
+        classes.add(new Class("1", "UC"));
         List<Vod> list = new ArrayList<>();
         String pic = "https://androidcatvodspider.netlify.app/wechat.png";
         String name = "关注公众号";
@@ -27,6 +32,32 @@ public class Introduce extends Spider {
         String name2 = "本接口不收费，请不要付费，谢谢！";
         list.add(new Vod("https://androidcatvodspider.netlify.app/wechat.png", name2, pic2));
         return Result.string(list);
+    }
+
+
+    @Override
+    public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) {
+        List<Vod> vodList = new ArrayList<>();
+        //UC
+        if (tid.equals("1")) {
+            String pic = "https://androidcatvodspider.netlify.app/wechat.png";
+            String name = "点击设置Token";
+            vodList.add(new Vod("UCToken", name, pic));
+        }
+        return Result.get().vod(vodList).string();
+    }
+
+    @Override
+    public String detailContent(List<String> ids) throws Exception {
+        String vodId = ids.get(0);
+
+        //UC Token 扫码
+        if (vodId.equals("UCToken")) {
+            QRCodeHandler qrCodeHandler = new QRCodeHandler();
+            qrCodeHandler.startUC_TOKENScan();
+        }
+        Vod item = new Vod();
+        return Result.string(item);
     }
 
 }
